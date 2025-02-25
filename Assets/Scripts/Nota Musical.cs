ae855946e4976546f59de58948af5b0effa8a838
UzCaroco <netoluiz805@hotmail.com>
2025-02-25 00:36:45 -0300

    notas

--- Assets/Scripts/Nota Musical.cs
+++ Assets/Scripts/Nota Musical.cs
@@ -7,6 +7,22 @@ public class NotaMusical : MonoBehaviour
 
     public float velocidade;
 
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("FaseAtual") == 1)
+        {
+            velocidade = 2;
+        }
+        else if (PlayerPrefs.GetInt("FaseAtual") == 2)
+        {
+            velocidade = 2;
+        }
+        else if (PlayerPrefs.GetInt("FaseAtual") == 3)
+        {
+            velocidade = 1.5f;
+        }
+    }
+
     void Update()
     {
         transform.Translate(Vector3.right * velocidade * Time.deltaTime);
