d2f89f57adb13964d7947c1538e6bfe215e4b233
Rosalie Alves <rosalieslv@gmail.com>
2025-08-07 00:02:17 -0300

    ...

--- Assets/Scripts/PlanetaPrincipal/PortalController.cs
+++ Assets/Scripts/PlanetaPrincipal/PortalController.cs
@@ -13,8 +13,8 @@ public class PortalController : MonoBehaviour
     private void Start()
     {
         Debug.Log("Fase: " + PlayerPrefs.GetInt("FaseAtual"));
-        
-        if (PlayerPrefs.GetInt("FaseAtual", 1) >= 3)
+
+        if (PlayerPrefs.GetInt("ConclusaoDaFase") >= 3)
         {
             for (int i = 0; i < gameObjMaterialForChange.Length; i++)
             {
